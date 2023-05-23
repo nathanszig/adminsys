c5d9b7cec2d49728e30c6e4cb2886aa6dba1b6c8
nathanszig <70863695+nathanszig@users.noreply.github.com>
2023-05-23 11:45:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express(); // Calls the method "express()" and assigns it's output t
 
 app.get("/", (req, res) => { // Creates sort of a listener for when there are "GET" requests to the "/" (root) path. Takes in req (request) and res (response)
 
-    res.send("Hello world!!!!"); // For the response, send a string "Hello World!"
+    res.send("Hello worlde"); // For the response, send a string "Hello World!"
 
 });
 
